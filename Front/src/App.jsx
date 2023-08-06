877b00adb304b800ecc54c10925b7e00346349fe
Max Prandi <maxikaled@gmail.com>
2023-08-06 12:18:02 -0300

    cree el proyecto react vite

---
+++ Front/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+
+    </BrowserRouter>
+
+
+  )
+}
+
+export default App
---
+++ Front/src/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+    return(
+        <div>
+        <h1>Nature Tunes</h1>
+        </div>
+    )
+}
+
+export { Home }
