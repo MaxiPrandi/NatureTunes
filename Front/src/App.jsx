4a9e51170eb6b60c2e908c05e55e8ad5fc760480
Max Prandi <maxikaled@gmail.com>
2023-08-08 15:24:00 -0300

    subo estructura web

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,6 +3,7 @@ import '/public/styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './home'
 import { NavBar } from './partials/Navbar'
+import { Footer } from './partials/Footer'
 import { Seccion1 } from './Seccion1'
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/Seccion1' element={<Seccion1 />}></Route>
       </Routes>
-
+    <Footer />
     </BrowserRouter>
 
 
