511437a53f747f7cdd9d215d0f97c48f7a2185a8
Max Prandi <maxikaled@gmail.com>
2023-08-27 18:13:28 -0300

    subo navbar responsive

--- Front/src/partials/Navbar.jsx
+++ Front/src/partials/Navbar.jsx
@@ -18,7 +18,7 @@ function NavBar() {
             <Row className="mt-2 mx-5">
                 <Col xs={12} md={2} className="mt-3">
                     <Link to="/" className="text-light">
-                        <img src="/src/assets/images/default2.png" className="img-thumbnail border-0 p-1 d-flex" alt="" />
+                        <img src="/./src/assets/images/default2.png" className="img-thumbnail border-0 p-1 d-flex" alt="" />
                     </Link>
                 </Col>
                 {/* buscador */}
