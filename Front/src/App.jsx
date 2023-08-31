ec5caaa3418ed6ac0c10b1eb5b3a3e48cd76ebda
Max Prandi <maxikaled@gmail.com>
2023-08-31 19:12:10 -0300

    hice formularios

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -5,6 +5,10 @@ import { Home } from './home'
 import { NavBar } from './partials/Navbar'
 import { Footer } from './partials/Footer'
 import { Seccion1 } from './Seccion1'
+import { Condicion } from './condiciones'
+import { Login } from './Login'
+import { Cart } from './Cart'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +19,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/Seccion1' element={<Seccion1 />}></Route>
+        <Route path='/Condiciones' element={<Condicion/>}></Route>
+        <Route path='/Login' element={<Login/>}></Route>
+        <Route path='/Cart' element={<Cart/>}></Route>
       </Routes>
     <Footer />
     </BrowserRouter>
---
+++ Front/src/condiciones.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import '/public/styles/App.css'
+import { Container, Row, Col, Image } from "react-bootstrap"
+import { Link } from "react-router-dom";
+
+const Condicion = () => {
+
+    return (
+        <>
+            <Container fluid>
+                <div>
+                    <a href="https://api.whatsapp.com/send?phone=1122334455" className="btn-wsp " target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="36" fill="currentColor" className="bi bi-whatsapp" viewBox="0 1 16 16">
+                            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+                        </svg> </a>
+                </div>
+                <Row>
+                    <h3 className="bg-dark text-light pt-5 pb-5 fw-bold">TERMNINOS Y CONDICIONES</h3>
+                    <div>
+                        <p className="textCond text-start">
+                            Términos y Condiciones del Sitio <br /><br />
+
+                            Bienvenido a la tienda oficial de Nature Tunes!<br /><br />
+
+                            Al ingresar a esta página Web y navegar, realizar cualquier compra o transacción Usted está aceptando los términos y condiciones que debajo se detallan y las Políticas de Privacidad y Protección de datos del Sitio.  en adelante en conjunto los “T&C”).
+
+                            El Sitio y sus funcionalidades sólo estarán disponibles para personas que tengan capacidad legal para contratar.
+
+                            El Sitio y la marca registrada Nature Tunes comprenden una licencia exclusiva legalmente otorgada a C C Agro & Tec. S.A., CUIT 30-71087919-9, con domicilio en Jerónimo Salguero 2731, Piso 6to Dpto. 61 CABA. <br /><br />
+
+                            Registro de Usuario<br /><br />
+
+                            Al Ingresar al Sitio puede ser solicitado su registro como Usuario para poder avanzar con la compra de productos a través del mismo. Para ello, deberá ingresar su mail, crear una contraseña y completar una serie de datos básicos, incluyendo sin limitar, Documento Nacional de Identidad, dirección, Nombre y Apellido completo, teléfono, entre otros. Dichos datos serán tomados como verídicos y en formato de declaración jurada por parte del Usuario.  Asimismo, aplicarán todas las Políticas de Privacidad y Protección de Datos que forman parte integrante de los Términos y Condiciones.
+
+                            Recuerde que el uso y administración de su Usuario y contraseña queda bajo su absoluta responsabilidad y recomendamos no compartirla con terceros ni utilizarla en lugares o dispositivos que no sean confiables.
+
+                            Por favor, en caso de que su Usuario y Contraseña pueda estar en riesgo o haya sido conocido por terceros, el Usuario tendrá la obligación de informar a nuestro mail: soporte@naturetunes.com.ar en forma inmediata para proceder a dar de baja el Usuario y evitar cualquier inconveniente.
+
+                            La Empresa se reserva el derecho de suspender, y/o rechazar cualquier solicitud de registración o de cancelar una registración previamente aceptada, sin que esté obligado a comunicar o exponer las razones de su decisión y sin que ello genere algún derecho a indemnización o resarcimiento.
+
+                            En el supuesto la suspensión o inhabilitación de un Usuario, los Productos que hubiera adquirido y las ofertas realizadas serán removidos del sistema, sin lugar a reclamo alguno a favor del Usuario.
+
+                            Cualquier uso indebido de su Registro en este Sitio, intromisión, tentativa o actividad violatoria o contraria a las leyes sobre derecho de propiedad intelectual y/o a las prohibiciones estipuladas en estos T&C darán derecho a la Empresa de iniciar los reclamos y acciones legales para la reparación de cualquier daño y/o la indemnización por los daños ocasionados.
+
+                            Compra y Pagos de Productos<br /><br />
+
+                            Una vez seleccionado el o los productos, y utilizar la forma de pago seleccionada, La Empresa contará con un plazo de cuarenta y ocho  (48)  horas para  aceptar la transacción sujeta a confirmación, ya sea para confirmar el stock disponible del producto, corroborar el pago realizado o los datos del Usuario que generó la operación.. Toda aceptación de oferta por parte del Usuario quedará sujeta a la condición suspensiva de que la Empresa valide la transacción.
+
+                            Dentro del procesos de aceptación y finalización del proceso de compra, Usted será derivado a una página web de un tercero, que otorgará un plataforma legalmente autorizada para transacciones de pagos y donde depositará los datos correspondientes a su tarjeta de crédito y demás requisitos que cada una de las empresas recaudadoras de pago solicite oportunamente. Todas las plataformas de pago elegidas por nuestra empresa han sido seleccionadas con un proceso exhaustivo, no obstante, estas operaciones quedan fuera de la órbita de tratamiento de datos y gestión de nuestra empresa por lo que no procesará datos de tarjetas de crédito ni cuentas bancarias. El sitio web de terceros puede contener enlaces a otros sitios totalmente ajenos e independientes a la Empresa.
+
+                            Constituye una condición suspensiva para cada compra efectuada en el Sitio, la validación de datos y el crédito disponible según informe cada plataforma o tercero designado para el procesamiento de datos del pago. Asimismo , todas las compras realizadas en el Sitio están sujetas a disponibilidad de stock.
+
+                            Zonas de Entregas. Envíos<br /><br />
+
+                            Nuestros productos pueden ser enviados a todo el territorio de la República Argentina, mediante transporte externo a la Empresa. Los tiempos de envíos y recepción de los Productos podrán variar según los tiempos y condiciones de envíos contratados especialmente o por la zona geográfica seleccionada para la entrega del Producto.
+
+                            Los plazos elegidos para el despacho y entrega, rigen desde que la Empresa haya confirmado la orden de compra y el medio de pago utilizado, y se consideran días hábiles para el cumplimiento de dicho plazo.
+
+                            El Costo de envío del/los Productos seleccionados siempre estarán a cargo del cliente, pudiendo en algunas ocasiones, según disponibilidad y posibilidad de opción, elegir un tipo de envío “normal” y otro “rápido” que tendrán distintos costos y se actualizarán constantemente en la sección del carrito de compras.
+
+                            Al recibir el Producto, es importante que el comprador o autorizado a su recepción, revise efectivamente el producto y cumpla con los requisitos de edad que indique el transportista, siendo que la recepción implicará para la Empresa la recepción completa y correcta del Producto oportunamente seleccionado.
+
+                            Información sobre Precios y Stock de Producto<br /><br />
+
+                            Los precios, stock y productos exhibidos en el Sitio aplican únicamente a compras mediante el canal online, en el momento de selección de los mismos y sujetos a confirmación de compra al finalizar la operación.
+
+                            En caso de que el precio de un producto, por un error involuntario del Sitio, se haya publicado un valor muy inferior a su costo (por ejemplo, precio de venta $ 1, $10, $100 ó $ 0), la empresa se reserva el derecho de dejar sin efecto la transacción y entendiendo la buena fe por parte del Consumidor del Producto.
+
+                            En caso de corroborar una falta de stock del o los productos seleccionados y confirmados con pago por Usted, la Empresa realizará la inmediata cancelación de la compra y gestión de la devolución del dinero en caso de corresponder.
+
+                            Cambios y devoluciones<br /><br />
+
+                            Derecho de retractación del consentimiento<br /><br />
+
+                            En cumplimiento con la Ley de Defensa al Consumidor, El Usuario o consumidor podrá retractarse de la compra realizada dentro del plazo de diez (10) días contados a partir de la entrega del producto o de la celebración del contrato, lo último que ocurra. Pasado ese plazo, la Empresa podrá buscar la mejor solución posible en pos de la satisfacción del consumidor, sin que esto sea obligatorio para ella.
+
+                            Dentro de dicho plazo, el consumidor podrá proceder a la devolución por medio de remisión del producto adquirido, en perfecto estado de uso y conservación, junto con sus etiquetas, manuales, embalaje y accesorios originales, también en perfecto estado, y acompañando su factura respectiva. Una vez recibido el producto por la Empresa y examinado el mismo, se procederá a la restitución de la cantidad abonada en concepto de precio, por el mismo medio en que fue abonado el producto. La devolución del artículo por el consumidor no supondrá penalización alguna para la Empresa ni para el consumidor. La devolución deberá ser inmediatamente informada a la Empresa y  la operación de devolución del precio total quedará sujeta a la revisión del Producto al momento de ser recibido por la Empresa, siendo a su discrecionalidad la imposibilidad de devolución si hay daños, roturas o no está en perfecto estado tanto el Producto como su embalaje y accesorios, o de descontar un porcentaje del valor del mismo al consumidor.<br /><br />
+
+                            Garantía de Producto<br /><br />
+
+                            El consumidor de todos nuestros productos posee una garantía legal por los defectos o vicios de cualquier índole, cuando afecten la identidad entre lo ofrecido y lo entregado o su correcto funcionamiento en condiciones normales y uso adecuado del Producto.
+
+                            Recuerde que la garantía legal tendrá vigencia por SEIS (6) meses a partir de la entrega.
+                            del mismo, por lo que el consumidor tendrá la obligación de informar a la Empresa tan pronto le sea posible y dentro del plazo legal, de la situación para coordinar el retiro del Producto para su reposición o reparación en caso de que corresponda.
+
+                            Para hacer uso de su Garantía de Producto es importante que cuente con:
+
+                            • Factura de compra donde se mencione el producto.
+
+                            • Producto con accesorios, manuales, y sin daños, roturas o modificaciones realizadas por el propio Consumidor.
+
+                            Productos de Outlet<br />
+
+                            A excepción del plazo de garantía arriba mencionado y en concordancia con la normativa vigente en el ámbito de Defensa al Consumidor, todo Producto ofrecido en la sección OUTLET del Sitio, por ser que posee alguna, deficiencia, falta de embalaje original o de algún accesorio o por ser un producto reconstruido cuenta igualmente con una garantía  legal por TRES (3) meses desde su compra . Dicha garantía es solamente  aplicable específicamente al  Producto siempre que no fuere en relación a la falla, falta o deficiencia efectivamente comunicada al momento de ofrecer el Producto.
+
+                            <br /><br />
+                            Actualizaciones<br /><br />
+
+                            El Sito podrá modificar en cualquier momento los Términos y Condiciones Generales, los que será informado al Usuario en su siguiente ingreso a la Web para que pueda prestar su consentimiento o solicite la cancelación de su Usuario en caso de ser necesario.
+
+                            Los presentes Términos y Condiciones se aplicará para todas las acciones y operaciones dentro del territorio de la República Argentina, y las partes ante cualquier conflicto se someten a la jurisdicción de los Tribunales Ordinarios de la Capital Federal.-_
+
+                            El uso de este sitio web implica la aceptación de los Términos y Condiciones y de las Políticas de Privacidad, Protección de Datos de CC AGRO & TEC S.A., como operador y autorizado a explotar la marca y dominio Nature Tunes en la República Argentina.
+
+                            Todas las imágenes de los productos que se ofrecen mediante esta web son a modo ilustrativo.
+
+                            La venta de cualquiera de los productos publicados está sujeta a la verificación de stock.
+                        </p>
+
+                    </div>
+
+                </Row>
+
+
+
+
+            </Container>
+
+        </>
+    )
+}
+
+
+export { Condicion }
